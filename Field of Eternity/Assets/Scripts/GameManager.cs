cc93004759d8295ed00c1356335c9623e80a207a
Matija Huremović <matija.huremovic@gmail.com>
2020-01-12 18:14:21 +0100

    Basic spawner mockup and unit models upload

---
+++ Field of Eternity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+
+    private Spawner spawner;
+
+    private void Start()
+    {
+        spawner = GetComponent<Spawner>();
+
+        CheckForMissingComponents();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            spawner.SelectLane(0);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            spawner.SelectLane(1);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            spawner.SelectLane(2);
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            spawner.SpawnUnit(0);
+        }
+    }
+
+    private void CheckForMissingComponents()
+    {
+        if(spawner == null)
+        {
+            Debug.LogError("GameManager.cs - Missing Component: Spawner");
+        }
+    }
+}
---
+++ Field of Eternity/Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] playerSpawnPoints;
+    [SerializeField]
+    private GameObject[] AISpawnPoints;
+    [SerializeField]
+    private GameObject[] spawnableUnits;
+
+    private int activePlayerLaneIndex = 0;
+
+    public void SelectLane(int laneIndex)
+    {
+        activePlayerLaneIndex = laneIndex;
+    }
+
+    public void SpawnUnit(int unitIndex)
+    {
+        Instantiate(spawnableUnits[unitIndex], playerSpawnPoints[activePlayerLaneIndex].transform.position, Quaternion.identity);
+    }
+}
