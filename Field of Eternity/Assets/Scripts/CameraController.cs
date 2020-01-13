f76d569bc1a8336f499f0d2143ea3c8860cc92ec
Matija Huremović <matija.huremovic@gmail.com>
2020-01-13 16:25:06 +0100

    Camera controls added

---
+++ Field of Eternity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float panSpeed = 30f;
+    [SerializeField]
+    private float panBorderThickness = 10f;
+    [SerializeField]
+    private Vector2 panClampLimit;
+    [SerializeField]
+    private float scrollSpeed = 20f;
+    [SerializeField]
+    private float minY = 20f;
+    [SerializeField]
+    private float maxY = 100f;
+
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if(Input.GetKey(KeyCode.W) || Input.mousePosition.y >= (Screen.height - panBorderThickness))
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= (Screen.width - panBorderThickness))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * Time.deltaTime * 100;
+        
+        pos.x = Mathf.Clamp(pos.x, -panClampLimit.x, panClampLimit.x);
+        pos.z = Mathf.Clamp(pos.z, -panClampLimit.y, panClampLimit.y / 2);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        transform.position = pos;
+    }
+}
