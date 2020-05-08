7c5ec4b9a98c506b04158da6caf9b640632941ed
Matija Huremović <matija.huremovic@gmail.com>
2020-05-09 00:26:12 +0200

    MoverSystem completed

---
+++ Field of Eternity/Assets/Scripts/ECS/LevelUpSystem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+public class LevelUpSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref LevelComponent levelComponent) =>
+        {
+            levelComponent.level += 1f * Time.DeltaTime;
+        });
+    }
+}
