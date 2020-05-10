8aab4b8488f672d25da7ea8f53e7723ad1e72ec0
Matija Huremović <matija.huremovic@gmail.com>
2020-05-10 16:58:50 +0200

    Basic movement added to units

--- Field of Eternity/Assets/Scripts/ECS/MovementSystem.cs
+++ Field of Eternity/Assets/Scripts/ECS/MovementSystem.cs
@@ -1,9 +1,13 @@
 ﻿using Unity.Entities;
+using Unity.Transforms;
 
 public class MovementSystem : ComponentSystem
 {
     protected override void OnUpdate()
     {
-        
+        Entities.ForEach((ref MovementComponent movement, ref Translation translation) =>
+        {
+            translation.Value.x += movement.movementSpeed * movement.movementDirection * Time.DeltaTime;
+        });
     }
 }
