aba348333b1e4548f3603595bc4adbee25f23bf1
Matija Huremović <matija.huremovic@gmail.com>
2020-05-10 14:56:14 +0200

    ECS will now spawn multiple units, made the algorithm use only perfect squares

--- Field of Eternity/Assets/Scripts/ECS/UnitSpawner.cs
+++ Field of Eternity/Assets/Scripts/ECS/UnitSpawner.cs
@@ -20,7 +20,7 @@ public class UnitSpawner : MonoBehaviour
     [SerializeField]
     private int wavesBeforeIncrement = 3;
     [SerializeField]
-    private int[] spawnsInWave = new int[] { 1, 10, 25, 100 };
+    private int[] spawnsInWave = new int[] { 1, 9, 25, 100 };
     [SerializeField]
     private Transform[] zombieSpawnPoints;
     [SerializeField]
@@ -79,6 +79,10 @@ public class UnitSpawner : MonoBehaviour
         {
             SpawnSingleUnit();
         }
+        else
+        {
+            SpawnMultipleUnits(amount);
+        }
     }
 
     private void SpawnSingleUnit()
@@ -115,6 +119,69 @@ public class UnitSpawner : MonoBehaviour
                 satyrSpawnPoints[i].position.z)
             });
             entityManager.SetSharedComponentData(entity, new RenderMesh { mesh = mesh_unit02, material = material_unit02 });
+        }
+
+        zombieArray.Dispose();
+        satyrArray.Dispose();
+    }
+
+    private void SpawnMultipleUnits(int amount)
+    {
+        double root = Mathf.Sqrt(amount);
+        if(root % 1 != 0)
+        {
+            Debug.Log("UnitSpawner.cs: Amount is not a perfect square!");
+            return;
+        }
+
+        NativeArray<Entity> zombieArray = new NativeArray<Entity>(zombieSpawnPoints.Length * amount, Allocator.Temp);
+        NativeArray<Entity> satyrArray = new NativeArray<Entity>(satyrSpawnPoints.Length * amount, Allocator.Temp);
+
+        entityManager.CreateEntity(archetype_unit01, zombieArray);
+        entityManager.CreateEntity(archetype_unit02, satyrArray);
+
+        int currentIndex = 0;
+
+        for(int k = 0; k < zombieSpawnPoints.Length; k++)
+        {
+            for (int i = 0; i < root; i++)
+            {
+                for (int j = 0; j < root; j++)
+                {
+                    Entity entity = zombieArray[currentIndex];
+
+                    entityManager.SetSharedComponentData(entity, new RenderMesh { mesh = mesh_unit01, material = material_unit01 });
+                    entityManager.SetComponentData(entity, new Translation
+                    {
+                        Value =
+                        new float3(zombieSpawnPoints[k].position.x + (i * 2), zombieSpawnPoints[k].position.y, zombieSpawnPoints[k].position.z + (j * 2))
+                    });
+
+                    currentIndex++;
+                }
+            }
+        }
+
+        currentIndex = 0;
+
+        for (int k = 0; k < satyrSpawnPoints.Length; k++)
+        {
+            for (int i = 0; i < root; i++)
+            {
+                for (int j = 0; j < root; j++)
+                {
+                    Entity entity = satyrArray[currentIndex];
+
+                    entityManager.SetSharedComponentData(entity, new RenderMesh { mesh = mesh_unit02, material = material_unit02 });
+                    entityManager.SetComponentData(entity, new Translation
+                    {
+                        Value =
+                        new float3(satyrSpawnPoints[k].position.x + (i * 2), satyrSpawnPoints[k].position.y, satyrSpawnPoints[k].position.z + (j * 2))
+                    });
+
+                    currentIndex++;
+                }
+            }
         }
 
         zombieArray.Dispose();
