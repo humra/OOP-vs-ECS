abfd94a83f1ba9e621fc39350a7f0355172e5fc7
Matija Huremović <matija.huremovic@gmail.com>
2020-05-17 02:23:25 +0200

    Tweaked unit stats slightly

--- Field of Eternity/Assets/Scripts/ECS/UnitStats.cs
+++ Field of Eternity/Assets/Scripts/ECS/UnitStats.cs
@@ -1,6 +1,6 @@
 ﻿public static class UnitStats
 {
-    public static int health = 10;
+    public static int health = 3;
     public static int damage = 1;
     public static float attackCooldown = 1;
     public static float engageRange = 10;
